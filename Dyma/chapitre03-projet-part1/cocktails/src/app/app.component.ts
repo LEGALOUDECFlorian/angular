4c42caa273a985ffdfed109916db61295f52f992
Florian LE GALOUDEC <pti.breton@hotmail.fr>
2024-05-14 23:47:54 +0200

    core: add cocktail-component and css

--- Dyma/chapitre03-projet-part1/cocktails/src/app/app.component.ts
+++ Dyma/chapitre03-projet-part1/cocktails/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './interface/cocktail.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cocktails';
+  cocktail: Cocktail = {
+    name: 'Mojito',
+    img: 'https://www.hangoverweekends.co.uk/uploads/images/mojito.jpg',
+    description: 'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.'
+  }
 }
